32af2705bb59aa6486e38941686897200db6192a
NAJIMx0 <sotanajim007@gmail.com>
2026-07-08 08:34:58 +0100

    initial architectur of systme

---
+++ plc-command-service/src/main/java/com/najim/plccommandservice/PlcCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.najim.plccommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlcCommandServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlcCommandServiceApplication.class, args);
+    }
+
+}
