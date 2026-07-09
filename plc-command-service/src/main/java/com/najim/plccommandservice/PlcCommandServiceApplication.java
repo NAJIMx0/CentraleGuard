f1ad23dbb04f17bcfeae830f1fbe2365e2490f0e
NAJIMx0 <sotanajim007@gmail.com>
2026-07-09 08:26:23 +0100

    add config and discovry services and integrate them

--- plc-command-service/src/main/java/com/najim/plccommandservice/PlcCommandServiceApplication.java
+++ plc-command-service/src/main/java/com/najim/plccommandservice/PlcCommandServiceApplication.java
@@ -2,8 +2,10 @@ package com.najim.plccommandservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlcCommandServiceApplication {
 
     public static void main(String[] args) {
